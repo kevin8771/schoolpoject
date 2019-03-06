985108866b10e9e24dbe7114ac690950d327ef9c
Kevin <kevinrotich423@gmail.com>
2019-03-06 11:40:20 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/4.2.1/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+    <style type="text/css">
+        #demo img{
+            width:100%;
+            height: 380px;
+        }
+
+        .container h1{
+            text-transform: uppercase;
+            color: green;
+
+            padding:16px;
+            text-align: center;
+
+
+        }
+        .noborder{
+            width: 100%;
+            border: 1px solid blue;
+            margin-top: 10px;
+        }
+    </style>
+
+</head>
+<body>
+<nav id="nav" class="navbar navbar-expand-md bg-warning navbar-light sticky-top">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">Kevin Rotich<i class="fa fa-khanda"></i></a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="School Project/services.php">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="FAQs.php">FAQs</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="School Project/login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="School Project/register.php">Register</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<!-- end section nav -->
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
+
+    <!-- The slideshow -->
+    <div class="carousel-inner .row width 100%">
+        <div class="carousel-item active">
+            <img src="images/6.jpg" alt="Los Angeles">
+            <div class="carousel-caption">
+                <h3>Django Doctor</h3>
+                <p>I love Python. Oops, not snakes!</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="images/5.jpg" alt="Chicago">
+            <div class="carousel-caption">
+                <h3>PHP Lord</h3>
+                <p>I love PHP as well</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="images/4.jpg" alt="New York">
+            <div class="carousel-caption">
+                <h3>Javascript Ninja</h3>
+                <p>I'm in Love with JS. I call it JSicca.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </a>
+
+</div>
+</div>
+<div class="container">
+    <h1>What we do</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+
+<div class="container=fluid  noborder">
+    <div class="row text-center">
+        <div class="col-4">
+            <i class="fa fa-phone"></i>
+            <h4>kenyans</h4>
+            <p>We believe in success</p>
+        </div>
+        <div class="col-4">
+            <i class="fa fa-phone"></i>
+            <h4>kenyans</h4>
+            <p>Success begins with personal efforts</p>
+        </div>
+        <div class="col-">
+            <i class="fa fa-phone"></i>
+            <h4>kenyans</h4>
+            <p>Believe in yourself always</p>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<script type="text/javascript" src="bootstrap/4.2.1/js/jquery.js"></script>
+<!-- <script type="text/javascript" src="bootstrap/4.2.1/popper.js"></script> -->
+<script type="text/javascript" src="bootstrap/4.2.1/js/bootstrap.js"></script>
+</body>
+</html<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8462550285462!2d36.798554315157894!3d-1.2647876359608348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1741d324c663%3A0x7fd3e7ccd9e69154!2seMobilis+Mobile+Technology+Training+Institute!5e0!3m2!1sen!2ske!4v1549609293879" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>>
